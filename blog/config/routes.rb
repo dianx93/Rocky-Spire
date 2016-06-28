0def0f216aa3dc6a4b644ee0437d0a703d6248b9
DAlgma <dianx93@gmail.com>
2016-06-28 15:27:16 +0300

    article, New article form with submission(kinda)

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :articles
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
@@ -55,4 +56,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
 end
